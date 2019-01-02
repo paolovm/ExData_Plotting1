0ef72adff5934b23c416ae4b18f57dce43c5a3c1
Paolo <paolo.malafaia@flexthink.com.au>
2019-01-01 22:50:49 -0200

    commiting solution v2

--- plot1.R
+++ plot1.R
@@ -1,8 +1,17 @@
+#open dataset
 col_class_vector <- c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric")
 dataset <- read.table("exdata_data_household_power_consumption/household_power_consumption.txt",sep=";",header=TRUE, colClasses = col_class_vector , na.strings = "?")
 dataset$Date <- as.Date( dataset$Date   , format="%d/%m/%Y")
+
+# subset data
 dataset_period <- dataset [dataset$Date>="2007-02-01" & dataset$Date<="2007-02-02" , ] 
+
+# initiate new png file
 png(file="plot1.png",width = 480, height = 480, units = "px")
+
+# generate graph
 hist(dataset_period$Global_active_power,col="red" , xlab="Global Active Power (kilowatts)" , main="Global Active Power")
+
+#turn off device
 dev.off() 
 
